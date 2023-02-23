4395488efe773585dd81f06f253d4f07dfd85d30
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-24 02:46:13 +0300

    Fix linter errors

--- spec/request/doctors_spec.rb
+++ spec/request/doctors_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Doctors', type: :request do
-    describe 'GET /index' do
-        it 'returns http success' do
-        get '/api/v1/doctors'
-        expect(response).to have_http_status(:success)
-        end
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/v1/doctors'
+      expect(response).to have_http_status(:success)
     end
-   
-    describe 'GET /show' do
-        it 'returns http success' do
-        get '/api/v1/doctors/1'
-        expect(response).to have_http_status(:success)
-        end
-    end
+  end
 
-    describe 'POST /create' do
-        it 'returns http success' do
-        post '/api/v1/doctors', params: {
-            doctor: {
-                "name":"Dr Steven Hobb",
-                "speciality":"Dentist",
-                "description":"Top dentist in the city of New York. Come get your dentals corrected in a single appointment. ",
-                "graduation":"2001-01-30",
-                "image":"hobb.png"
-            }
-          }
-        expect(response).to have_http_status(:success)
-        end
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/api/v1/doctors/1'
+      expect(response).to have_http_status(:success)
     end
-    
-    describe 'DELETE /destroy' do
-        it 'returns http success' do
-        delete '/api/v1/doctors/1'
-        expect(response).to have_http_status(:success)
-        end
+  end
+
+  describe 'POST /create' do
+    it 'returns http success' do
+      post '/api/v1/doctors', params: {
+        doctor: {
+          name: 'Dr Steven Hobb',
+          speciality: 'Dentist',
+          description: 'Top dentist in the city of New York. Come get your dentals corrected in a single appointment. ',
+          graduation: '2001-01-30',
+          image: 'hobb.png'
+        }
+      }
+      expect(response).to have_http_status(:success)
     end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'returns http success' do
+      delete '/api/v1/doctors/1'
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
