eb69540576aa194f3f22d789efa86ed36e39b259
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-23 23:55:33 +0300

    Fix rubocop errors

--- db/migrate/20230223203828_add_user_ref_to_reservations.rb
+++ db/migrate/20230223203828_add_user_ref_to_reservations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUserRefToReservations < ActiveRecord::Migration[7.0]
   def change
     add_reference :reservations, :user, null: false, foreign_key: true
--- db/migrate/20230223203844_add_doctor_ref_to_reservations.rb
+++ db/migrate/20230223203844_add_doctor_ref_to_reservations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDoctorRefToReservations < ActiveRecord::Migration[7.0]
   def change
     add_reference :reservations, :doctor, null: false, foreign_key: true
