6c4be45c199f0b7f8672c125525e602bcfd5acdc
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-24 02:33:00 +0300

    Fix linter errors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,34 +1,33 @@
 class Api::V1::DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+    render json: @doctors
+  end
 
-    def index
-        @doctors = Doctor.all
-        render json: @doctors
-    end
+  def show
+    @doctor = Doctor.find(params[:id])
+    render json: @doctor
+  end
 
-    def show
-        @doctor = Doctor.find(params[:id])
-        render json: @doctor
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      render json: 'Doctor created successfully'
+    else
+      render json: 'Doctor creation failed'
     end
+  end
 
-    def create
-        @doctor = Doctor.new(doctor_params)
-        if @doctor.save
-            render json: 'Doctor created successfully'
-        else
-            render json: 'Doctor creation failed'
-        end
-    end
+  def doctor_params
+    params.require(:doctor).permit(:name, :speciality, :description, :graduation, :image)
+  end
 
-    def doctor_params
-        params.require(:doctor).permit(:name, :speciality, :description, :graduation, :image)
+  def destroy
+    @doctor = Doctor.find(params[:id])
+    if @doctor.destroy
+      render json: 'Doctor deleted successfully'
+    else
+      render json: 'Doctor deletion failed'
     end
-
-    def destroy
-        @doctor = Doctor.find(params[:id])
-        if @doctor.destroy
-            render json: 'Doctor deleted successfully'
-        else
-            render json: 'Doctor deletion failed'
-        end
-    end
+  end
 end
