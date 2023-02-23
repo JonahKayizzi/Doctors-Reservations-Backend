dc8ea8d6dc73242512e7ac24ae055561311732fe
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-24 02:43:20 +0300

    Add request spec for doctors show endpoint

--- spec/request/doctors_spec.rb
+++ spec/request/doctors_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe 'Doctors', type: :request do
         expect(response).to have_http_status(:success)
         end
     end
+   
+    describe 'GET /show' do
+        it 'returns http success' do
+        get '/api/v1/doctors/1'
+        expect(response).to have_http_status(:success)
+        end
+    end
     
-
 end
