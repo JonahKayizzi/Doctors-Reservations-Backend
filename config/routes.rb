4b56dade0ece981ed2e6f5eb4163f5c65f117953
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-24 01:20:45 +0300

    Add routes for Doctors controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :doctors, only: [:index, :show, :create, :destroy]
+    end
+  end
 end
