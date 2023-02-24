bd6ce1ccb814bf603ae82a3bc9f47cce73ccf385
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-24 21:03:30 +0300

    Add integration spec for documenting doctors show API endpoint

--- spec/requests/api/doctors_spec.rb
+++ spec/requests/api/doctors_spec.rb
@@ -29,6 +29,30 @@ describe 'Doctors API' do
         end
     end
 
-    
+    path '/doctors/{id}' do
+        get 'Retrieves a doctor' do
+            tags 'Doctors'
+            produces 'application/json'
+            parameter name: :id, in: :path, type: :integer
+            response '200', 'doctor found' do
+                schema type: :object,
+                properties: {
+                    id: { type: :integer },
+                    name: { type: :string },
+                    speciality: { type: :string },
+                    description: { type: :string },
+                    graduation: { type: :string },
+                    image: { type: :string },
+                    created_at: { type: :string },
+                    updated_at: { type: :string }
+                },
+                required: [ 'id', 'name', 'speciality', 'description', 'graduation', 'image', 'created_at', 'updated_at' ]
+                run_test!
+            end
+            response '404', 'doctor not found' do
+                run_test!
+            end
+        end
+    end
 end
 
