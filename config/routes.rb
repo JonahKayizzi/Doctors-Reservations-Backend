121b694e6978dc057fe4eb11c2b8f05d55fe3961
camilovelag <camilo.vela.galindo@gmail.com>
2023-02-27 15:40:05 -0500

    Modify reservations#index to display reservations per user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :reservations, only: [:index, :new, :create] do
+      resources :users, only: [:create] do
+        resources :reservations, only: [:index, :new, :create] do
+        end
       end
     end
   end
@@ -12,12 +14,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :doctors, only: [:index, :show, :create, :destroy]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create]
     end
   end
 
