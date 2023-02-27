c2454c93def0db67830a04c633050f7a5dc2cc13
Cesar Valencia <107327344+cvalencia1991@users.noreply.github.com>
2023-02-27 11:56:31 -0500

    Update doctor.rb
    Minimize the description of the Doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ApplicationRecord
   has_many :reservations, dependent: :destroy
   validates :name, presence: true
   validates :speciality, presence: true
-  validates :description, presence: true, length: { minimum: 50, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 25, maximum: 300 }
   validates :graduation, presence: true
   validates :image, presence: true
 end
